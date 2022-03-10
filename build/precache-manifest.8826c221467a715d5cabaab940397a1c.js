c2c3da3d5c9c52ba8aa6402740551cd3da11bca5
Haitham Abdulaziz <49244741+H14m@users.noreply.github.com>
2022-03-10 03:18:46 +0300

    Add files via upload

---
+++ build/precache-manifest.8826c221467a715d5cabaab940397a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a49e0803f49e85ce9aee",
+    "url": "/static/css/main.0768c1ae.chunk.css"
+  },
+  {
+    "revision": "a49e0803f49e85ce9aee",
+    "url": "/static/js/main.a49e0803.chunk.js"
+  },
+  {
+    "revision": "f673cadc833bc0f0f0d9",
+    "url": "/static/js/1.f673cadc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b325916a05eb1409345a66d77abcc748",
+    "url": "/static/media/home.b325916a.jpg"
+  },
+  {
+    "revision": "d0b50b929f9ea7098550baa62ff8673a",
+    "url": "/index.html"
+  }
+];
